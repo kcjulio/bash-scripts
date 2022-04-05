26c71d29d57abcf23c56b40750fa1d24dfdb4fe4
Julio Cesar Kochhann <juliokochhann@hotmail.com>
2022-04-05 17:25:27 -0300

    Update cfg-install

--- cfg-install.sh
+++ cfg-install.sh
@@ -1,14 +1,25 @@
 git clone --bare https://github.com/kcjulio/dotfiles.git $HOME/.cfg
+
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
-mkdir -p .config-backup
-config checkout
-if [ $? = 0 ]; then
-  echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
-config checkout
+
+mkdir -p $HOME/.config-backup
+
+err=$(config checkout 2>&1)
+
+if [ -n "$err" ]
+then
+    echo "Backing up pre-existing dot files."
+
+    echo -e $err | grep -Eo "\.\w+" | xargs -I{} mv $HOME/{} $HOME/.config-backup/{}
+
+    config checkout
+fi
+
+echo "Checked out config."
+
 config config status.showUntrackedFiles no
+
+git clone https://github.com/dracula/gnome-terminal.git ../gnome-terminal
+/bin/bash ../gnome-terminal/install.sh
